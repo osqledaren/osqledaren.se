53564c430a219ca2080921b346fc3b49a5e34217
MaxKrog <krog.max@gmail.com>
2014-12-27 14:24:15 +0100

    Debugging MAMP. No actual changes made

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+  //FIXED-HEADER SCRIPTS
 	var currentScroll = 0;
 	var headerHeight = $("#header").height();
 
@@ -6,7 +8,8 @@ $(document).ready(function(){
 
   	//If scrolled past header
   	if( headerHeight -  $(this).scrollTop() <=0){ 
-  		$("#header").css({position: 'fixed',height:headerHeight},500);
+  		$("#header").css({"position": 'fixed',"top":-1*headerHeight+"px"},500);
+      console.log("Hej");
   	}
 
   	//If scrolled to top of page
@@ -16,8 +19,41 @@ $(document).ready(function(){
 
   	//If user is at the buttom of the page.
   	if($(window).scrollTop() + $(window).height() == $(document).height()) {
-       $("#header").css({position:"fixed",height:headerHeight});
+       $("#header").css({position:"fixed"});
    	}
   	
+
+
   });
+
+  //SEARCHBOX-SCRIPTS
+  $('.search_icon').click(function() {
+  if ( !$('.search_field').is(':visible') ) {
+    /* Display the search field */
+    $('.search_icon').animate({right: '13'}, 500);
+    $('.search_field').show('slide', { direction: 'right' }, 500);  
+  }
+});
+$('body').click(function(evt){    
+  /* Hide form when anywhere in body (except search form) is clicked */
+  if ( evt.target.class == 'search_form' )
+  return;
+  if( $(evt.target).closest('.search_form').length )
+  return;
+  
+  /* Hide the search field */  
+  if ( $('.search_field').is(':visible') ) {
+    $('.search_icon').animate({right: '0'}, 500);
+    $('.search_field').hide('slide', { direction: 'right' }, 500);
+  }
+});
+
+
+$("#logo").hover(function() {
+  $("#logo #stripe").addClass('loaded');
+});
+
+
+
+
 });
--- footer.php
+++ footer.php
@@ -14,39 +14,7 @@
 </footer><!-- /#footer -->
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script type="text/javascript" src="./assets/js/everything-min.js"></script> <!-- Compiled libraries JS-->
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+<script type="text/javascript" src="./assets/js/everything-min.js"></script> <!-- jQuery,Underscore,Backbone,Handlebars Minified -->
 <script type="text/javascript" src="./assets/js/header.js"></script> <!-- denna hör till headern. (Men vill inte blocka DOM-load med Javascript.) -->
 <script type="text/javascript" src="./assets/js/footer.js"></script> <!-- Footerns javascript -->
-<script type="text/x-handlebars-template">
-	</script>
 
-
-
-<script type="text/javascript">
-$('.search_icon').click(function() {
-	if ( !$('.search_field').is(':visible') ) {
-		/* Display the search field */
-		$('.search_icon').animate({right: '13'}, 500);
-		$('.search_field').show('slide', { direction: 'right' }, 500);	
-	}
-});
-$('body').click(function(evt){    
-	/* Hide form when anywhere in body (except search form) is clicked */
-	if ( evt.target.class == 'search_form' )
-	return;
-	if( $(evt.target).closest('.search_form').length )
-	return;
-	
-	/* Hide the search field */  
-	if ( $('.search_field').is(':visible') ) {
-		$('.search_icon').animate({right: '0'}, 500);
-		$('.search_field').hide('slide', { direction: 'right' }, 500);
-	}
-});
-
-
-$("#logo").hover(function() {
-	$("#logo #stripe").addClass('loaded');
-});
-</script>
