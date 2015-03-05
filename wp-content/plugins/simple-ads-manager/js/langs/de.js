aa02f81b6db02118b67d885ed60a180fdc28a5a9
MaxKrog <krog.max@gmail.com>
2015-03-05 20:49:26 +0000

    Removed simple-ads-manager from git

--- wp-content/plugins/simple-ads-manager/js/langs/de.js
+++
@@ -1,8 +0,0 @@
-tinyMCE.addI18n("de,samb", {
-  'Insert Advertisement': 'Insert Advertisement',
-  'Select Advertisement Object': 'Select Advertisement Object',
-  'Insert Single Ad': 'Insert Single Ad',
-  'Insert Ads Place': 'Insert Ads Place',
-  'Insert Ads Zone': 'Insert Ads Zone',
-  'Insert Ads Block': 'Insert Ads Block'
-});
