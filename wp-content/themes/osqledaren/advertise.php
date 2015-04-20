5f7e29433e995c92bb4b50aad3af6bbbb1a09ead
Nicolas Lindberg <nicolas@nicolas.se>
2015-04-20 16:53:25 +0200

    Fixed advertise page bug

--- wp-content/themes/osqledaren/advertise.php
+++ wp-content/themes/osqledaren/advertise.php
@@ -7,39 +7,53 @@
 
 get_header(); ?>
 
-	<article id="advertise" class="page_content container">
-		
-		<?php while ( have_posts() ) : the_post(); ?>
-			<div class="content">
-				<div class="row">
-					<div class="padding">
-							
-						<h1><?php the_title(); ?></h1>
-						
-						<?php the_content(); ?>	
-					</div>
+<article class="page_content container">
+	
+	<?php while ( have_posts() ) : the_post(); ?>
+		<div class="content">
+			<div class="row">
+				<div class="padding">
+					<h1><?php the_title(); ?></h1>
+					
+					<?php the_content(); ?>	
 				</div>
-			</div><!-- /.content -->
-		<?php endwhile; ?>
-	
-	</article><!-- /#about -->
-	
-	<div id="covers">
-		<div class="wrapper">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol141502.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol141501.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol141500.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131404.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131403.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131402.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131401.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131400.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121305.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121304.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121303.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121302.jpg" width="190" height="237">
-			<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121301.jpg" width="190" height="237">
-		</div>
+			</div>
+		</div><!-- /.content -->
+	<?php endwhile; ?>
+
+</article><!-- /#about -->
+
+<div id="covers">
+	<div class="wrapper">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol141502.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol141501.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol141500.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131404.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131403.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131402.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131401.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131400.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121305.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121304.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121303.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121302.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121301.jpg" width="190" height="237">
+		
+		<!-- Extra images for filling -->
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol141502.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol141501.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol141500.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131404.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131403.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131402.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131401.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol131400.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121305.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121304.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121303.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121302.jpg" width="190" height="237">
+		<img src="<?php echo bloginfo('template_url'); ?>/assets/img/covers/ol121301.jpg" width="190" height="237">
 	</div>
+</div>
 
 <?php get_footer(); ?>
