c227cccf01276a5b6464bbed6f751df4034943dc
Arvid Sätterkvist <arvidsat@gmail.com>
2015-11-25 20:30:54 +0100

    Lock and thumbnails

--- wp-content/themes/osqledaren/functions.php
+++ wp-content/themes/osqledaren/functions.php
@@ -405,6 +405,29 @@ add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
         $adv = $christmas -$daysTillSunday - 21;
         return $adv;
 }
+function youtube_id_from_url($url) {
+    $pattern = 
+        '%^# Match any youtube URL
+        (?:https?://)?  # Optional scheme. Either http or https
+        (?:www\.)?      # Optional www subdomain
+        (?:             # Group host alternatives
+          youtu\.be/    # Either youtu.be,
+        | youtube\.com  # or youtube.com
+          (?:           # Group path alternatives
+            /embed/     # Either /embed/
+          | /v/         # or /v/
+          | /watch\?v=  # or /watch\?v=
+          )             # End path alternatives.
+        )               # End host alternatives.
+        ([\w-]{10,12})  # Allow 10-12 for 11 char youtube id.
+        $%x'
+        ;
+    $result = preg_match($pattern, $url, $matches);
+    if (false !== $result) {
+        return $matches[1];
+    }
+    return false;
+}
 
 
 
